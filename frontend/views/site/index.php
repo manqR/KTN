447e341bdeeb82db7f4c5d5d1ba9d51a67f5e65b
Muhamad Adinugraha <manqrdeveloper@gmail.com>
2018-09-06 20:02:34 +0700

    frontend-2

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,75 +4,90 @@
 <div class="slider-container rev_slider_wrapper">
     <div id="revolutionSlider" class="slider rev_slider" data-version="5.4.7" data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'gridwidth': 1170, 'gridheight': 500, 'responsiveLevels': [4096,1200,992,500]}">
         <ul>
-            <li data-transition="fade">
-                <img src="img/slides/slide-corporate-3.jpg"  
-                    alt=""
-                    data-bgposition="center center" 
-                    data-bgfit="cover" 
-                    data-bgrepeat="no-repeat" 
-                    class="rev-slidebg">
-                <div class="tp-caption"
-                    data-x="['177','177','center','center']" data-hoffset="['0','0','-150','-220']"
-                    data-y="180"
-                    data-start="1000"
-                    data-transform_in="x:[-300%];opacity:0;s:500;"><img src="img/slides/slide-title-border-light.png" alt=""></div>
-                <div class="tp-caption top-label"
-                    data-x="['227','227','center','center']"
-                    data-y="172"
-                    data-fontsize="['24','24','24','36']"	
-                    data-start="500"
-                    data-transform_in="y:[-300%];opacity:0;s:500;">DO YOU NEED A NEW</div>
-                <div class="tp-caption"
-                    data-x="['480','480','center','center']" data-hoffset="['0','0','150','220']"
-                    data-y="180"
-                    data-start="1000"
-                    data-transform_in="x:[300%];opacity:0;s:500;"><img src="img/slides/slide-title-border-light.png" alt=""></div>
-                <div class="tp-caption main-label"
-                    data-x="['135','135','center','center']"
-                    data-y="['210','210','210','230']"
-                    data-start="1500"
-                    data-whitespace="nowrap"	
-                    data-fontsize="['62','62','62','82']"					 
-                    data-transform_in="y:[100%];s:500;"
-                    data-transform_out="opacity:0;s:500;"
-                    data-mask_in="x:0px;y:0px;">WEB DESIGN?</div>
-                <div class="tp-caption bottom-label"
-                    data-x="['185','185','center','center']"
-                    data-y="['280','280','280','315']"
-                    data-start="2000"
-                    data-fontsize="['20','20','20','30']"
-                    data-transform_in="y:[100%];opacity:0;s:500;">Check out our options and features.</div>
-            </li>
-            <li data-transition="fade">
-                <img src="img/slides/slide-corporate-4.jpg"  
-                    alt=""
-                    data-bgposition="center center" 
-                    data-bgfit="cover" 
-                    data-bgrepeat="no-repeat" 
-                    class="rev-slidebg">
-                <div class="tp-caption featured-label"
-                    data-x="center"
-                    data-y="210"
-                    data-start="500"
-                    data-fontsize="['52','52','52','82']"
-                    style="z-index: 5"
-                    data-transform_in="y:[100%];s:500;"
-                    data-transform_out="opacity:0;s:500;">WELCOME TO PT-KTN</div>
-                <div class="tp-caption bottom-label"
-                    data-x="center"
-                    data-y="['270','270','270','290']"
-                    data-start="1000"
-                    data-fontsize="['23','23','23','38']"
-                    data-transform_idle="o:1;"
-                    data-transform_in="y:[100%];z:0;rZ:-35deg;sX:1;sY:1;skX:0;skY:0;s:600;e:Power4.easeInOut;"
-                    data-transform_out="opacity:0;s:500;"
-                    data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-                    data-splitin="chars" 
-                    data-splitout="none" 
-                    data-responsive_offset="on"
-                    style="font-size: 23px; line-height: 30px;"
-                    data-elementdelay="0.05">The #1 Selling HTML Site Template on ThemeForest</div>
-            </li>
+            <?php
+                $odd = 0;
+                foreach($slider as $sliders):
+                
+                $odd++;                    
+                if($odd % 2 == 0){
+            ?>
+                <li data-transition="fade">
+                    <img src="img/slides/slide-corporate-3.jpg"  
+                        alt=""
+                        data-bgposition="center center" 
+                        data-bgfit="cover" 
+                        data-bgrepeat="no-repeat" 
+                        class="rev-slidebg">
+                    <div class="tp-caption"
+                        data-x="['177','177','center','center']" data-hoffset="['0','0','-150','-220']"
+                        data-y="180"
+                        data-start="1000"
+                        data-transform_in="x:[-300%];opacity:0;s:500;"><img src="img/slides/slide-title-border-light.png" alt=""></div>
+                    <div class="tp-caption top-label"
+                        data-x="['227','227','center','center']"
+                        data-y="172"
+                        data-fontsize="['24','24','24','36']"	
+                        data-start="500"
+                        data-transform_in="y:[-300%];opacity:0;s:500;">DO YOU NEED A NEW</div>
+                    <div class="tp-caption"
+                        data-x="['480','480','center','center']" data-hoffset="['0','0','150','220']"
+                        data-y="180"
+                        data-start="1000"
+                        data-transform_in="x:[300%];opacity:0;s:500;"><img src="img/slides/slide-title-border-light.png" alt=""></div>
+                    <div class="tp-caption main-label"
+                        data-x="['135','135','center','center']"
+                        data-y="['210','210','210','230']"
+                        data-start="1500"
+                        data-whitespace="nowrap"	
+                        data-fontsize="['62','62','62','82']"					 
+                        data-transform_in="y:[100%];s:500;"
+                        data-transform_out="opacity:0;s:500;"
+                        data-mask_in="x:0px;y:0px;">WEB DESIGN?</div>
+                    <div class="tp-caption bottom-label"
+                        data-x="['185','185','center','center']"
+                        data-y="['280','280','280','315']"
+                        data-start="2000"
+                        data-fontsize="['20','20','20','30']"
+                        data-transform_in="y:[100%];opacity:0;s:500;">Check out our options and features.</div>
+                </li>
+            <?php
+                }else{
+            ?>
+
+                <li data-transition="fade">
+                    <img src="img/slides/slide-corporate-4.jpg"  
+                        alt=""
+                        data-bgposition="center center" 
+                        data-bgfit="cover" 
+                        data-bgrepeat="no-repeat" 
+                        class="rev-slidebg">
+                    <div class="tp-caption featured-label"
+                        data-x="center"
+                        data-y="210"
+                        data-start="500"
+                        data-fontsize="['52','52','52','82']"
+                        style="z-index: 5"
+                        data-transform_in="y:[100%];s:500;"
+                        data-transform_out="opacity:0;s:500;">WELCOME TO PT-KTN</div>
+                    <div class="tp-caption bottom-label"
+                        data-x="center"
+                        data-y="['270','270','270','290']"
+                        data-start="1000"
+                        data-fontsize="['23','23','23','38']"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];z:0;rZ:-35deg;sX:1;sY:1;skX:0;skY:0;s:600;e:Power4.easeInOut;"
+                        data-transform_out="opacity:0;s:500;"
+                        data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                        data-splitin="chars" 
+                        data-splitout="none" 
+                        data-responsive_offset="on"
+                        style="font-size: 23px; line-height: 30px;"
+                        data-elementdelay="0.05">The #1 Selling HTML Site Template on ThemeForest</div>
+                </li>
+            <?php } 
+                endforeach;
+            ?>
+            
         </ul>
     </div>
 </div>
@@ -101,6 +116,9 @@
         </div>
     </div>
     <div class="row">
+        <?php
+            foreach($layanan as $layanans):
+        ?>
         <div class="col-lg-4">
             <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
                 <div class="feature-box-icon">
@@ -112,97 +130,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
-                <div class="feature-box-icon">
-                    <i class="icon-layers icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Sliders</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
-                <div class="feature-box-icon">
-                    <i class="icon-calculator icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">HTML5</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-lg-3">
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
-                <div class="feature-box-icon">
-                    <i class="icon-star icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Icons</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
-                <div class="feature-box-icon">
-                    <i class="icon-drop icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Colors</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
-                <div class="feature-box-icon">
-                    <i class="icon-mouse icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Buttons</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-lg-3 mb-5">
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="600">
-                <div class="feature-box-icon">
-                    <i class="icon-screen-desktop icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Lightboxes</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="600">
-                <div class="feature-box-icon">
-                    <i class="icon-energy icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Elements</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="feature-box feature-box-style-2 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="600">
-                <div class="feature-box-icon">
-                    <i class="icon-social-youtube icons"></i>
-                </div>
-                <div class="feature-box-info">
-                    <h4 class="mb-2">Videos</h4>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
-                </div>
-            </div>
+        <?php endforeach; ?>
         </div>
     </div>
 </div>
